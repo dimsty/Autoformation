36583a44b6b8797de449250ea09180266b00bcc0
dimsty <64203426+dimsty@users.noreply.github.com>
2020-09-03 16:24:42 +0200

    Add files via upload

---
+++ Autoformation/credentials.php
@@ -0,0 +1,6 @@
+<?php 
+
+define("HOST", "localhost");
+define("DBNAME", "biblio");
+define("DBUSER", "root");
+define("DBPASSWORD", "Super");
---
+++ Autoformation/supprimerNationalite.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+require_once('connectionDb.php');
+$num = $_GET['num'];
+
+
+
+  $req = $myDb->prepare("delete from nationalite where num = :num");
+  $req->bindParam(':num', $num);
+
+$nb = $req->execute();
+
+
+if ($nb == 1) {
+    $_SESSION['message']=["success" => "La nationalité a bien été supprimée"];
+
+} else {
+    $_SESSION['message']=["danger" => "La nationalité n'a pas été supprimée"];
+
+}
+header('Location: listeNationalites.php');
+exit();
